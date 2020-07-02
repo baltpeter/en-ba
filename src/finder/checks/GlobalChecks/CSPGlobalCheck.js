c3973c672ffe00086350b50663086fd1d42551f3
Benjamin Altpeter <benjamin@oldpeter.de>
2020-07-02 13:20:53 +0200

    Modify CSPGlobalCheck to collect the statistics we are interested in

--- src/finder/checks/GlobalChecks/CSPGlobalCheck.js
+++ src/finder/checks/GlobalChecks/CSPGlobalCheck.js
@@ -2,44 +2,68 @@ import * as attributes from '../../attributes';
 import * as csp from '@doyensec/csp-evaluator';
 
 export default class CSPGlobalCheck {
-
-  constructor() {
-    this.id = "CSP_GLOBAL_CHECK";
-    this.description = {  NO_CSP: "No CSP has been detected in the target application",
-      MAYBE_WEAK_CSP: "One or more CSP directives detected seems to be vulnerable",
-      WEAK_CSP: "One or more CSP directives detected are vulnerable" };
-    this.depends = ["CSPJSCheck", "CSPHTMLCheck"];
-    this.shortenedURL = "https://git.io/JeuMe";
-  }
-
-  async perform(issues) {
-    var cspIssues = issues.filter(e => e.id === 'CSP_JS_CHECK' || e.id === 'CSP_HTML_CHECK');
-    var otherIssues = issues.filter(e => e.id !== 'CSP_JS_CHECK' && e.id !== 'CSP_HTML_CHECK');
-    if (cspIssues.length === 0) {
-      // No CSP detected
-      issues.push({ file: "N/A", location: {line: 0, column: 0}, id: this.id, description: this.description.NO_CSP, shortenedURL: this.shortenedURL, severity: attributes.severity.MEDIUM, confidence: attributes.confidence.CERTAIN, manualReview: false });
-      return issues;
-    } else {
-      // There is a CSP set
-      var confidence = 0; 
-      for (var cspIssue of cspIssues) {
-        var parser = new csp.CspParser(cspIssue.properties.CSPstring);
-        var evaluator = new csp.CspEvaluator(parser.csp, csp.Version.CSP3);
-        var findings = evaluator.evaluate();
-        for (var finding of findings)
-          if (finding.severity === csp.severities.HIGH || finding.severity === csp.severities.MEDIUM)
-            confidence = 2;
-          else if (finding.severity === csp.severities.HIGH_MAYBE || finding.severity === csp.severities.MEDIUM_MAYBE)
-            if (confidence < 2) confidence = 1;
-      }
-
-      if (confidence === 2) 
-        otherIssues.push({ file: cspIssues[0].file, location: cspIssues[0].location, id: this.id, description: this.description.WEAK_CSP, shortenedURL: this.shortenedURL, severity: attributes.severity.LOW, confidence: attributes.confidence.CERTAIN, sample: cspIssue.properties.CSPstring, manualReview: false });
-      if (confidence === 1)
-        otherIssues.push({ file: cspIssues[0].file, location: cspIssues[0].location, id: this.id, description: this.description.MAYBE_WEAK_CSP, shortenedURL: this.shortenedURL, severity: attributes.severity.LOW, confidence: attributes.confidence.FIRM, sample: cspIssue.properties.CSPstring, manualReview: true });
-
-
-      return otherIssues;
+    constructor() {
+        this.id = 'CSP_GLOBAL_CHECK';
+        this.description = {
+            NO_CSP: 'No CSP has been detected in the target application',
+            MAYBE_WEAK_CSP: 'One or more CSP directives detected seems to be vulnerable',
+            WEAK_CSP: 'One or more CSP directives detected are vulnerable',
+        };
+        this.depends = ['CSPJSCheck', 'CSPHTMLCheck'];
+        this.shortenedURL = 'https://git.io/JeuMe';
     }
-  }
+
+    async perform(issues) {
+        const cspIssues = issues.filter((e) => e.id === 'CSP_JS_CHECK' || e.id === 'CSP_HTML_CHECK');
+        const otherIssues = issues.filter((e) => e.id !== 'CSP_JS_CHECK' && e.id !== 'CSP_HTML_CHECK');
+
+        const properties = {
+            csp_list: [],
+            no_csp_found: false,
+            num_strong_csps: 0,
+            num_maybe_weak_csps: 0,
+            num_weak_csps: 0,
+        };
+        if (cspIssues.length === 0) {
+            // No CSP detected
+            properties.no_csp_found = true;
+        } else {
+            // There is a CSP set
+            for (const cspIssue of cspIssues) {
+                properties.csp_list.push(cspIssue.properties.CSPstring);
+
+                const parser = new csp.CspParser(cspIssue.properties.CSPstring);
+                const evaluator = new csp.CspEvaluator(parser.csp, csp.Version.CSP3);
+                const findings = evaluator.evaluate();
+                let worst_grade = 'strong';
+
+                for (const finding of findings) {
+                    console.log(finding);
+                    if (finding.severity === csp.severities.HIGH || finding.severity === csp.severities.MEDIUM) {
+                        worst_grade = 'weak';
+                    } else if (
+                        (finding.severity === csp.severities.HIGH_MAYBE ||
+                            finding.severity === csp.severities.MEDIUM_MAYBE) &&
+                        worst_grade == 'strong'
+                    ) {
+                        worst_grade = 'maybe_weak';
+                    }
+                }
+
+                if (worst_grade === 'strong') properties.num_strong_csps++;
+                else if (worst_grade === 'maybe_weak') properties.num_maybe_weak_csps++;
+                else if (worst_grade === 'weak') properties.num_weak_csps++;
+            }
+        }
+
+        const report = {
+            file: 'N/A',
+            location: { line: 0, column: 0 },
+            id: this.id,
+            description: this.description.NO_CSP,
+            shortenedURL: this.shortenedURL,
+            properties,
+        };
+        return [...otherIssues, report];
+    }
 }
